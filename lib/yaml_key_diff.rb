6f67e524092cab8c307fa18bf1a7d240ea77213d
summertree128 <natsukinoue530000@gmail.com>
2021-07-03 02:49:14 +0900

    implement first version

--- lib/yaml_key_diff.rb
+++ lib/yaml_key_diff.rb
@@ -1,5 +1,48 @@
+require 'yaml'
+require 'diffy'
+
 class YamlKeyDiff
-  def self.hi
-    "hello, world"
+  def self.run(key:, yml_1:, yml_2:)
+    partial_1 = dfs(yml_1, key)
+    partial_2 = dfs(yml_2, key)
+
+    Diffy::Diff.new(
+      YAML.dump(nested_sort_hash(partial_1)),
+      YAML.dump(nested_sort_hash(partial_2))
+    )
   end
+
+  private
+
+    def self.dfs(hash, key)
+      keys = hash.keys.sort_by(&:to_s)
+      keys.each do |k|
+        return hash[k] if k == key
+        dfs(hash[k], key) if hash[k].is_a?(Hash)
+      end
+      nil
+    end
+
+    def self.nested_sort_hash(hash)
+      hash.each do |k, v|
+        if v.is_a?(Hash)
+          hash[k] = nested_sort_hash(v)
+        elsif v.is_a?(Array)
+          hash[k] = nested_sort_array(v)
+        end
+      end
+      hash.sort.to_h
+    end
+
+    def self.nested_sort_array(array)
+      array.each_with_index do |v, i|
+        if v.is_a?(Hash)
+          array[i] = nested_sort_hash(v)
+        elsif v.is_a?(Array)
+          array[i] = nested_sort_array(v)
+        end
+      end
+      array.sort_by(&:to_s)
+    end
 end
+
--- test/test_yaml_key_diff.rb
+++ test/test_yaml_key_diff.rb
@@ -2,8 +2,34 @@ require 'minitest/autorun'
 require 'yaml_key_diff'
 
 class YamlKeyDiffTest < Minitest::Test
-  def test_hi
-    assert_equal "hello, world",
-      YamlKeyDiff.hi
+  def test_run
+    yml_1 = <<~YAML_EOT
+    key1:
+      nested_key1: aaa
+    key2:
+      nested_key2: bbb
+    YAML_EOT
+
+    yml_2 = <<~YAML_EOT
+    key1:
+      nested_key1: aaa
+    key2:
+      nested_key2: ccc
+    YAML_EOT
+
+    yml_1 = YAML.load(yml_1)
+    yml_2 = YAML.load(yml_2)
+
+    assert_equal "",
+      YamlKeyDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
+
+    expected_diff = <<~EXPECTED_DIFF
+     ---
+    -nested_key2: bbb
+    +nested_key2: ccc
+    EXPECTED_DIFF
+
+    assert_equal expected_diff,
+      YamlKeyDiff.run(key: 'key2', yml_1: yml_1, yml_2: yml_2).to_s
   end
 end
