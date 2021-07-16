da367e744e5e8ffec3490cc48d4a97922ce91b5e
summertree128 <natsukinoue530000@gmail.com>
2021-07-16 14:25:43 +0900

    fix bug

--- lib/yaml_search_diff.rb
+++ lib/yaml_search_diff.rb
@@ -2,49 +2,64 @@ require 'yaml'
 require 'diffy'
 
 class YamlSearchDiff
-  def self.run(key:, yml_1:, yml_2:)
-    return "" unless yml_1.is_a?(Hash) && yml_2.is_a?(Hash)
+  class << self
+    def run(key:, yml_1:, yml_2:)
+      return "" unless yml_1.is_a?(Hash) && yml_2.is_a?(Hash)
 
-    partial_1 = dfs(yml_1, key)
-    partial_2 = dfs(yml_2, key)
+      searched_1 = catch(:has_key) { dfs(yml_1, key) }
+      searched_2 = catch(:has_key) { dfs(yml_2, key) }
 
-    Diffy::Diff.new(
-      YAML.dump(nested_sort_hash(partial_1)),
-      YAML.dump(nested_sort_hash(partial_2))
-    )
+      sorted_1 = sort_yml(searched_1)
+      sorted_2 = sort_yml(searched_2)
+
+      Diffy::Diff.new(
+        YAML.dump(sorted_1),
+        YAML.dump(sorted_2)
+      )
+    end
+
+    private
+
+      def dfs(hash, key)
+        keys = hash.keys.sort_by(&:to_s)
+        keys.each do |k|
+          throw :has_key, hash[k] if k == key
+          dfs(hash[k], key) if hash[k].is_a?(Hash)
+        end
+        {}
+      end
+
+      def sort_yml(yml)
+        if yml.is_a?(Hash)
+          nested_sort_hash(yml)
+        elsif yml.is_a?(Array)
+          nested_sort_array(yml)
+        else
+          yml
+        end
+      end
+
+      def nested_sort_hash(hash)
+        hash.each do |k, v|
+          if v.is_a?(Hash)
+            hash[k] = nested_sort_hash(v)
+          elsif v.is_a?(Array)
+            hash[k] = nested_sort_array(v)
+          end
+        end
+        hash.sort_by { |k, v| k.to_s }.to_h
+      end
+
+      def nested_sort_array(array)
+        array.each_with_index do |v, i|
+          if v.is_a?(Hash)
+            array[i] = nested_sort_hash(v)
+          elsif v.is_a?(Array)
+            array[i] = nested_sort_array(v)
+          end
+        end
+        array.sort_by(&:to_s)
+      end
   end
-
-  private
-
-    def self.dfs(hash, key)
-      keys = hash.keys.sort_by(&:to_s)
-      keys.each do |k|
-        return hash[k] if k == key
-        dfs(hash[k], key) if hash[k].is_a?(Hash)
-      end
-      nil
-    end
-
-    def self.nested_sort_hash(hash)
-      hash.each do |k, v|
-        if v.is_a?(Hash)
-          hash[k] = nested_sort_hash(v)
-        elsif v.is_a?(Array)
-          hash[k] = nested_sort_array(v)
-        end
-      end
-      hash.sort.to_h
-    end
-
-    def self.nested_sort_array(array)
-      array.each_with_index do |v, i|
-        if v.is_a?(Hash)
-          array[i] = nested_sort_hash(v)
-        elsif v.is_a?(Array)
-          array[i] = nested_sort_array(v)
-        end
-      end
-      array.sort_by(&:to_s)
-    end
 end
 
--- test/test_yaml_search_diff.rb
+++ test/test_yaml_search_diff.rb
@@ -4,33 +4,76 @@ require 'yaml_search_diff'
 class YamlSearchDiffTest < Minitest::Test
   def test_run
     str_1 = <<~YAML_EOT
-    key1:
-      nested_key1: aaa
-    key2:
-      nested_key2: bbb
-    YAML_EOT
+      key1:
+        nested_key1: aaa
+      key2:
+        nested_key2: bbb
+      key3:
+        nested_key3:
+          nested_nested_key:
+            - AAAAA
+            - BBBBB
+            - CCCCC
+          nested_nested_key2:
+            - nested_nested_nested_key: XXXX
+            - nested_nested_nested_key2: YYYY
+          null: ~
+          true: yes
+          false: no
+      YAML_EOT
 
     str_2 = <<~YAML_EOT
-    key1:
-      nested_key1: aaa
-    key2:
-      nested_key2: ccc
-    YAML_EOT
+      key1:
+        nested_key1: aaa
+      key2:
+        nested_key2: ccc
+      key3:
+        nested_key3:
+          nested_nested_key:
+            - CCCCC
+            - DDDDD
+            - EEEEE
+            - FFFFF
+      YAML_EOT
 
     yml_1 = YAML.load(str_1)
     yml_2 = YAML.load(str_2)
 
     assert_equal "",
+    YamlSearchDiff.run(key: 'non-existence-key', yml_1: yml_1, yml_2: yml_2).to_s
+
+    assert_equal "",
       YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
 
-    expected_diff = <<~EXPECTED_DIFF
+    expected_diff_key1 = <<~EXPECTED_DIFF
      ---
     -nested_key2: bbb
     +nested_key2: ccc
     EXPECTED_DIFF
 
-    assert_equal expected_diff,
+    assert_equal expected_diff_key1,
       YamlSearchDiff.run(key: 'key2', yml_1: yml_1, yml_2: yml_2).to_s
+
+
+    expected_diff_nested_key3 = <<~EXPECTED_DIFF
+     ---
+    -! '':
+    -false: false
+     nested_nested_key:
+    -- AAAAA
+    -- BBBBB
+     - CCCCC
+    -nested_nested_key2:
+    -- nested_nested_nested_key: XXXX
+    -- nested_nested_nested_key2: YYYY
+    -true: true
+    +- DDDDD
+    +- EEEEE
+    +- FFFFF
+    EXPECTED_DIFF
+
+    assert_equal expected_diff_nested_key3,
+      YamlSearchDiff.run(key: 'nested_key3', yml_1: yml_1, yml_2: yml_2).to_s
   end
 
   def test_empty_file
@@ -40,6 +83,44 @@ class YamlSearchDiffTest < Minitest::Test
     yml_2 = YAML.load(str_2)
 
     assert_equal "",
+      YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
+  end
+
+  def test_nested_array
+    str_1 = <<~YAML_EOT
+    key1:
+      - AAA
+      - BBB
+      - [ aaa, bbb, ccc]
+    YAML_EOT
+
+    str_2 = <<~YAML_EOT
+    key1:
+      - [ ddd, eee, [ fff, ggg ]]
+      - AAA
+      - [ hhh ]
+      - [ bbb, ccc ]
+    YAML_EOT
+
+    yml_1 = YAML.load(str_1)
+    yml_2 = YAML.load(str_2)
+
+    expected_diff = <<~EXPECTED_DIFF
+     ---
+     - AAA
+    -- BBB
+    -- - aaa
+    -  - bbb
+    +- - bbb
+       - ccc
+    +- - hhh
+    +- - - fff
+    +    - ggg
+    +  - ddd
+    +  - eee
+    EXPECTED_DIFF
+
+    assert_equal expected_diff,
       YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
   end
 end
