d73ea0ad8489dc10c5fed43a5cfddf4db5918af0
summertree128 <natsukinoue530000@gmail.com>
2021-07-27 01:08:16 +0900

    Handle data types other than string

--- lib/yaml_search_diff.rb
+++ lib/yaml_search_diff.rb
@@ -2,76 +2,80 @@ require 'yaml'
 require 'diffy'
 
 class YamlSearchDiff
-  class << self
-    def run(key:, yml_1:, yml_2:)
-      return "" unless yml_1.is_a?(Hash) && yml_2.is_a?(Hash)
 
-      searched_1 = key.include?(':') ? search_dig(key, yml_1) : search(key, yml_1)
-      searched_2 = key.include?(':') ? search_dig(key, yml_2) : search(key, yml_2)
+  def initialize
+    @scalar_scanner = Psych::ScalarScanner.new(Psych::ClassLoader.new)
+  end
 
-      Diffy::Diff.new(
-        YAML.dump(searched_1),
-        YAML.dump(searched_2)
-      )
+  def run(key:, yml_1:, yml_2:)
+    return "" unless yml_1.is_a?(Hash) && yml_2.is_a?(Hash)
+
+    searched_1 = key.include?(':') ? search_dig(key, yml_1) : search(key, yml_1)
+    searched_2 = key.include?(':') ? search_dig(key, yml_2) : search(key, yml_2)
+
+    Diffy::Diff.new(
+      YAML.dump(searched_1),
+      YAML.dump(searched_2)
+    )
+  end
+
+  private
+
+    def search(key, yml)
+      tokenized_key = @scalar_scanner.tokenize(key)
+      searched = catch(:has_key) { dfs(yml, tokenized_key) }
+      sort_yml(searched)
     end
 
-    private
+    def search_dig(key, yml)
+      tokenized_keys = key.split(':').map {|k| @scalar_scanner.tokenize(k) }
+      first_key = tokenized_keys.first
 
-      def search(key, yml)
-        searched = catch(:has_key) { dfs(yml, key) }
-        sort_yml(searched)
+      searched = catch(:has_key) { dfs(yml, first_key) }
+      digged = searched.dig(*tokenized_keys[1..])
+
+      sort_yml(digged)
+    end
+
+    def dfs(hash, key)
+      keys = hash.keys.sort_by(&:to_s)
+      keys.each do |k|
+        throw :has_key, hash[k] if k == key
+        dfs(hash[k], key) if hash[k].is_a?(Hash)
       end
+      {}
+    end
 
-      def search_dig(key, yml)
-        splitted_keys = key.split(':')
-        first_key = splitted_keys.first
-
-        searched = catch(:has_key) { dfs(yml, first_key) }
-        digged = searched.dig(*splitted_keys[1..])
-
-        sort_yml(digged)
+    def sort_yml(yml)
+      if yml.is_a?(Hash)
+        nested_sort_hash(yml)
+      elsif yml.is_a?(Array)
+        nested_sort_array(yml)
+      else
+        yml
       end
+    end
 
-      def dfs(hash, key)
-        keys = hash.keys.sort_by(&:to_s)
-        keys.each do |k|
-          throw :has_key, hash[k] if k == key
-          dfs(hash[k], key) if hash[k].is_a?(Hash)
+    def nested_sort_hash(hash)
+      hash.each do |k, v|
+        if v.is_a?(Hash)
+          hash[k] = nested_sort_hash(v)
+        elsif v.is_a?(Array)
+          hash[k] = nested_sort_array(v)
         end
-        {}
       end
+      hash.sort_by { |k, v| k.to_s }.to_h
+    end
 
-      def sort_yml(yml)
-        if yml.is_a?(Hash)
-          nested_sort_hash(yml)
-        elsif yml.is_a?(Array)
-          nested_sort_array(yml)
-        else
-          yml
+    def nested_sort_array(array)
+      array.each_with_index do |v, i|
+        if v.is_a?(Hash)
+          array[i] = nested_sort_hash(v)
+        elsif v.is_a?(Array)
+          array[i] = nested_sort_array(v)
         end
       end
-
-      def nested_sort_hash(hash)
-        hash.each do |k, v|
-          if v.is_a?(Hash)
-            hash[k] = nested_sort_hash(v)
-          elsif v.is_a?(Array)
-            hash[k] = nested_sort_array(v)
-          end
-        end
-        hash.sort_by { |k, v| k.to_s }.to_h
-      end
-
-      def nested_sort_array(array)
-        array.each_with_index do |v, i|
-          if v.is_a?(Hash)
-            array[i] = nested_sort_hash(v)
-          elsif v.is_a?(Array)
-            array[i] = nested_sort_array(v)
-          end
-        end
-        array.sort_by(&:to_s)
-      end
-  end
+      array.sort_by(&:to_s)
+    end
 end
 
--- test/test_yaml_search_diff.rb
+++ test/test_yaml_search_diff.rb
@@ -2,6 +2,11 @@ require 'minitest/autorun'
 require 'yaml_search_diff'
 
 class YamlSearchDiffTest < Minitest::Test
+
+  def setup
+    @ysdiff = YamlSearchDiff.new
+  end
+
   def test_run
     str_1 = <<~YAML_EOT
       key1:
@@ -40,12 +45,12 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       "",
-      YamlSearchDiff.run(key: 'non-existence-key', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'non-existence-key', yml_1: yml_1, yml_2: yml_2).to_s
     )
 
     assert_equal(
       "",
-      YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
     )
 
     expected_diff_key1 = <<~EXPECTED_DIFF
@@ -56,7 +61,7 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       expected_diff_key1,
-      YamlSearchDiff.run(key: 'key2', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key2', yml_1: yml_1, yml_2: yml_2).to_s
     )
 
 
@@ -78,7 +83,7 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       expected_diff_nested_key3,
-      YamlSearchDiff.run(key: 'nested_key3', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'nested_key3', yml_1: yml_1, yml_2: yml_2).to_s
     )
   end
 
@@ -90,7 +95,7 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       "",
-      YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2)
+      @ysdiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2)
     ).to_s
   end
 
@@ -130,7 +135,7 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       expected_diff,
-      YamlSearchDiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key1', yml_1: yml_1, yml_2: yml_2).to_s
     )
   end
 
@@ -172,7 +177,7 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       expected_diff_1,
-      YamlSearchDiff.run(key: 'key1:nested_key1:nested_nested_key1', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key1:nested_key1:nested_nested_key1', yml_1: yml_1, yml_2: yml_2).to_s
     )
 
 # Comment out since this assertion fails on GitHub Actions only
@@ -183,7 +188,7 @@ class YamlSearchDiffTest < Minitest::Test
 
 #     assert_equal(
 #       expected_diff_2,
-#       YamlSearchDiff.run(key: 'key1:nested_key1:nested_nested_key2', yml_1: yml_1, yml_2: yml_2)
+#       @ysdiff.run(key: 'key1:nested_key1:nested_nested_key2', yml_1: yml_1, yml_2: yml_2)
 # .to_s
 #     )
 
@@ -195,14 +200,114 @@ class YamlSearchDiffTest < Minitest::Test
 
     assert_equal(
       expected_diff_3,
-      YamlSearchDiff.run(key: 'key2:nested_key1:nested_nested_key1', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key2:nested_key1:nested_nested_key1', yml_1: yml_1, yml_2: yml_2).to_s
     )
 
     expected_diff_4 = ''
 
     assert_equal(
       expected_diff_4,
-      YamlSearchDiff.run(key: 'key1:non-existence_key', yml_1: yml_1, yml_2: yml_2).to_s
+      @ysdiff.run(key: 'key1:non-existence_key', yml_1: yml_1, yml_2: yml_2).to_s
+    )
+  end
+
+  def test_integer_key
+    str_1 = <<~YAML_EOT
+    123:
+      - AAA
+      - BBB
+    456:
+      789:
+        - CCC
+        - DDD
+    YAML_EOT
+
+    str_2 = <<~YAML_EOT
+    123:
+      - BBB
+      - CCC
+    456:
+      789:
+        - 12345
+        - 67890
+    YAML_EOT
+
+    yml_1 = YAML.load(str_1)
+    yml_2 = YAML.load(str_2)
+
+    expected_diff_1 = <<~DIFF
+     ---
+    -- AAA
+     - BBB
+    +- CCC
+    DIFF
+
+    assert_equal(
+      expected_diff_1,
+      @ysdiff.run(key: '123', yml_1: yml_1, yml_2: yml_2).to_s
+    )
+
+    expected_diff_2 = <<~DIFF
+     ---
+    -- CCC
+    -- DDD
+    +- 12345
+    +- 67890
+    DIFF
+
+    assert_equal(
+      expected_diff_2,
+      @ysdiff.run(key: '456:789', yml_1: yml_1, yml_2: yml_2).to_s
+    )
+  end
+
+  def test_float_key
+    str_1 = <<~YAML_EOT
+    1.23:
+      - AAA
+      - BBB
+    4.56:
+      78.9:
+        - CCC
+        - DDD
+    YAML_EOT
+
+    str_2 = <<~YAML_EOT
+    1.23:
+      - BBB
+      - CCC
+    4.56:
+      78.9:
+        - 123.45
+        - 6.7890
+    YAML_EOT
+
+    yml_1 = YAML.load(str_1)
+    yml_2 = YAML.load(str_2)
+
+    expected_diff_1 = <<~DIFF
+     ---
+    -- AAA
+     - BBB
+    +- CCC
+    DIFF
+
+    assert_equal(
+      expected_diff_1,
+      @ysdiff.run(key: '1.23', yml_1: yml_1, yml_2: yml_2).to_s
+    )
+
+    expected_diff_2 = <<~DIFF
+     ---
+    -- CCC
+    -- DDD
+    +- 123.45
+    +- 6.789
+    DIFF
+
+    assert_equal(
+      expected_diff_2,
+      @ysdiff.run(key: '4.56:78.9', yml_1: yml_1, yml_2: yml_2).to_s
     )
   end
 end
